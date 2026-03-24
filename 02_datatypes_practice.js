0ccea300c97af1d2e5dd3d7f8d615ef4541a6fef
Fardous563 <fardousrahman563@gmail.com>
2026-03-24 00:30:03 -0700

    Added Advanced Data Types, Falsy values and Memory concepts

---
+++ 02_datatypes_practice.js
@@ -0,0 +1,36 @@
+//1.String
+
+let studentName = "Abir Ahmed"; 
+// ekhani "Abir Ahmed" holo String besuse eta cottation ar vetor ache
+
+
+//2.number
+let age = 15; 
+let monthlyFee = 1500.50; 
+// ১৫ হলো পূর্ণসংখ্যা (Integer) আর ১৫০০.৫০ হলো দশমিক সংখ্যা (Float)
+
+//3.Boolean
+let isPassed = true; 
+let isFailed = false;
+// এখানে শুধু দুটি অপশন— হয় সত্য (true), নয়তো মিথ্যা (false)
+
+
+//4.Undefined
+let upcomingEvent; 
+console.log(upcomingEvent); // আউটপুট আসবে: undefined
+
+//5.Null
+let userBio = null;
+// এখানে তুমি নিজে থেকে বলে দিচ্ছো যে, "এখন এখানে কিছুই নেই"
+
+//6.BigInt
+let hugeNumber = 9007199254740991n; 
+// সংখ্যার শেষে 'n' থাকা মানে এটি একটি BigInt
+
+//7.symbol
+let uniqueId =Symbol("id");
+//symbol akta unique value creat kore,jar kono duplicate thakte pare na,ja akta unique indentifier create kore
+
+
+//*typeof */
+console.log(typeof studentName);  //output;string 
